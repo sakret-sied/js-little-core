de4ff7ab6e005710f4e6a073166723d397221e13
SakretSied <sacretsid@gmail.com>
2023-11-07 14:42:33 +0400

    add content

--- little.core.js
+++ little.core.js
@@ -3,13 +3,18 @@ import Module from './module.js';
 export default class LittleCore {
   constructor() {
     this.container = document.querySelector('#app');
+
     this.selectorKey = this.constructor.name.replace('App', 'selector');
     this.selectorId = this.selectorKey.replace(/([A-Z])/g, '-$1').toLowerCase();
+    this.selector = this.#createSelectorOnDocument();
+
+    this.contentId = this.selectorId.replace('selector', 'content');
+    this.content = this.#createContentOnDocument();
   }
 
   setModules(modules) {
     const checkedModules = modules
-      .map((module) => [module.name, new module()])
+      .map((module) => [module.name, new module(this.content)])
       .filter((module) => module[1] instanceof Module);
     this.modules = new Map(checkedModules);
   }
@@ -19,33 +24,39 @@ export default class LittleCore {
       return;
     }
 
-    const selector = this.#addSelectorToDocument();
-    this.#addOptionsToSelector(selector);
-    this.#addEventListenerToSelector(selector);
+    this.#addOptionsToSelector();
+    this.#addEventListenerToSelector();
   }
 
-  #addSelectorToDocument() {
+  #createContentOnDocument() {
+    const content = document.createElement('div');
+    content.id = this.contentId;
+    this.container.insertAdjacentElement('beforeend', content);
+    return content;
+  }
+
+  #createSelectorOnDocument() {
     const selector = document.createElement('select');
     selector.id = this.selectorId;
-    this.container.insertAdjacentElement('beforeend', selector);
+    this.container.insertAdjacentElement('afterbegin', selector);
     return selector;
   }
 
-  #addOptionsToSelector(selector) {
+  #addOptionsToSelector() {
     const defaultSelector = this.#getDefaultSelector();
     this.modules.forEach((_, index) => {
       const option = document.createElement('option');
       option.id = option.innerText = index;
       option.selected = index === defaultSelector ? 'selected' : '';
-      selector.insertAdjacentElement('beforeend', option);
+      this.selector.insertAdjacentElement('beforeend', option);
     });
-
     this.modules.get(defaultSelector).execute();
   }
 
-  #addEventListenerToSelector(selector) {
-    selector.addEventListener('change', (e) => {
+  #addEventListenerToSelector() {
+    this.selector.addEventListener('change', (e) => {
       console.clear();
+      this.content.innerHTML = '';
       this.#setDefaultSelector(e.target.value);
       this.modules.get(e.target.value).execute();
     });
--- module.js
+++ module.js
@@ -1,3 +1,7 @@
 export default class Module {
+  constructor(content) {
+    this.content = content;
+  }
+
   execute() {}
 }
